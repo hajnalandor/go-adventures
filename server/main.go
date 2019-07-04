9dd80c2cc0e0e98742c2379a72a3ca94e22f0b8a
hajnalandor <hajnal.andor1993@gmail.com>
2019-07-04 11:53:03 +0200

    single endpoint and build struct step by step

--- server/main.go
+++ server/main.go
@@ -7,31 +7,47 @@ import (
 	"net/http"
 )
 
-type data struct {
-	Website            string    `json:"websiteurl"`
-	ResizeFrom         dimension `json:"resizeFrom"`
-	ResizeTo           dimension `json:"resizeTo"`
-	CopyAndPaste       bool      `json:"copyAndPaste"`
-	FormCompletionTime float64   `json:"formCompletionTime"`
+var data Data
+
+// Data result struct
+type Data struct {
+	WebsiteURL         string
+	SessionID          string
+	ResizeFrom         Dimension
+	ResizeTo           Dimension
+	CopyAndPaste       map[string]bool // map[fieldId]true
+	FormCompletionTime int             // Seconds
 }
 
-type dimension struct {
-	Width  int `json:"width"`
-	Height int `json:"height"`
+// Dimension of window
+type Dimension struct {
+	Width  string `json:"width"`
+	Height string `json:"height"`
+}
+
+// RequestBody struct
+type RequestBody struct {
+	EventType  string    `json:"eventType"`
+	WebsiteURL string    `json:"websiteUrl"`
+	SessionID  string    `json:"sessionId"`
+	Pasted     bool      `json:"pasted"`
+	FormID     string    `json:"formId"`
+	ResizeFrom Dimension `json:"resizeFrom"`
+	ResizeTo   Dimension `json:"resizeTo"`
+	Time       int       `json:"time"`
 }
 
 func main() {
+	data.CopyAndPaste = make(map[string]bool)
 	fmt.Println("starting...")
 	fmt.Println("server listening port is 8080...")
 	fmt.Println("waiting resize,copypaste or submit action...")
-	http.HandleFunc("/resize", HandleRequest)
-	http.HandleFunc("/paste", HandleRequest)
-	http.HandleFunc("/submit", HandleRequest)
-
+	http.HandleFunc("/send", HandleRequest)
 	http.ListenAndServe(":8080", nil)
 
 }
 
+// HandleRequest ...
 func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
 	enableCors(&w)
@@ -42,12 +58,57 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("err", err)
 	}
-	var data data
-	err = json.Unmarshal([]byte(body), &data)
+	var requestBody RequestBody
+	err = json.Unmarshal([]byte(body), &requestBody)
 	if err != nil {
 		fmt.Println("err", err)
 	}
+	switch requestBody.EventType {
+	case "timeTaken":
+		submit(requestBody)
+	case "copyAndPaste":
+		copyAndPaste(requestBody)
+	case "resize":
+		resize(requestBody)
+	}
+
 	fmt.Println(data)
+}
+
+func submit(requestBody RequestBody) {
+	if requestBody.Time != 0 {
+		data.FormCompletionTime = requestBody.Time
+	}
+	if requestBody.WebsiteURL != "" {
+		data.WebsiteURL = requestBody.WebsiteURL
+	}
+	if requestBody.SessionID != "" {
+		data.SessionID = requestBody.SessionID
+	}
+}
+
+func copyAndPaste(requestBody RequestBody) {
+	if requestBody.WebsiteURL != "" {
+		data.WebsiteURL = requestBody.WebsiteURL
+	}
+	if requestBody.SessionID != "" {
+		data.SessionID = requestBody.SessionID
+	}
+
+	data.CopyAndPaste[requestBody.FormID] = requestBody.Pasted
+
+}
+
+func resize(requestBody RequestBody) {
+	if requestBody.WebsiteURL != "" {
+		data.WebsiteURL = requestBody.WebsiteURL
+	}
+	if requestBody.SessionID != "" {
+		data.SessionID = requestBody.SessionID
+	}
+	data.ResizeFrom = requestBody.ResizeFrom
+	data.ResizeTo = requestBody.ResizeTo
+
 }
 
 func enableCors(w *http.ResponseWriter) {
